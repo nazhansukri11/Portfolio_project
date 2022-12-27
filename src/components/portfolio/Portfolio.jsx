153e6dade389c907ce46c2a0a662de9667adc815
nazhan github <nazhansukri11@gmail.com>
2022-12-28 01:15:49 +0800

    add my portfolio project

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./portfolio.css";
+import Menu from './Menu';
 
 const Portfolio=()=>{
+    const [items,setItems]=useState(Menu);
+    const filterItem=(categoryItem)=>{
+        const updateItems=Menu.filter((culElem)=>{
+            return culElem.category===categoryItem;
+        });
+        setItems(updateItems);
+    }
     return(
-        <div>Portfolio</div>
+        <section className="portfolio container section" id="portfolio">
+            <h2 className="section__title">My Portfolio</h2>
+
+            <div className="work__filters">
+                <span className="work__item" onClick={()=>setItems(Menu)}>Everything</span>
+                <span className="work__item" onClick={()=>filterItem("Creative")}>Web Application</span>
+                <span className="work__item" onClick={()=>filterItem("Art")}>Electronic</span>
+            </div>
+
+            <div className="work__container grid">
+                {items.map((elem)=>{
+                    const{id,image,title,category}=elem;
+                    return(
+                        <div className="work__card" key={id}>
+                            <div className="work__thumbnail">
+                                <img src={image} alt="" className="work__image" />
+                                <div className="work__mask"></div>
+                            </div>
+
+                            <span className="work__category">{category}</span>
+                            <h3 className="work__title">{title}</h3>
+                            <a href="#" className="work__button">
+                                <i className="icon-link work__button-icon"></i>
+                            </a>
+
+                        </div>
+                        )
+                })}
+
+            </div>
+        </section>
     )
 }
 
